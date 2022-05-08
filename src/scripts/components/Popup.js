250b50a65b0e964581b41834be1c6ccdc2ab0c88
Begunov Dmitry <begdv@yandex.ru>
2022-05-08 21:55:15 +0300

    fix: Исправления после второго ревью

--- src/scripts/components/Popup.js
+++ src/scripts/components/Popup.js
@@ -2,13 +2,14 @@ export default class Popup{
   constructor(popupSelector){
     this._popupSelector = popupSelector;
     this._popup = document.querySelector(popupSelector);
+    this._handleEscCloseBinded = (evt) => this._handleEscClose(evt);
   }  
   open(){
     this._popup.classList.add('popup_opened'); 
-    document.addEventListener('keydown', this._handleEscClose.bind(this));
+    document.addEventListener('keydown', this._handleEscCloseBinded);
   }
   close(){
-    document.removeEventListener('keydown', this._handleEscClose);
+    document.removeEventListener('keydown', this._handleEscCloseBinded);
     this._popup.classList.remove('popup_opened');
   }
   _handleClickPopup(evt){
@@ -19,9 +20,8 @@ export default class Popup{
   _handleEscClose(evt){
     if (evt.key === 'Escape') {
       this.close();
-    };      
-
-  }
+    }; 
+  }       
   setEventListeners(){
     this._popup.addEventListener('click', (evt) => this._handleClickPopup(evt));
   }
